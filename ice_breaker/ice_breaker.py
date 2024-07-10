fc711642aa1b1d1651fa6900ffd325a91bdef9ba
Kacper Stefański <kacper.stefanski99@gmail.com>
2024-07-10 17:38:42 +0200

    fix inferencing HF models
    small fixes and experimenting with huggingface api

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,46 +1,28 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain_community.llms import HuggingFaceHub
 from langchain.chains import LLMChain
-from langchain import HuggingFacePipeline
 import os
+import json
 
 import torch
 import transformers
 from transformers import AutoTokenizer
 from agents.lookup_linkedin_agent import lookup_linkedin
 
-from third_parties.linkedin import scrape_linkedin_profile, get_sample_linkedin_profile
+from third_parties.linkedin import scrape_linkedin_profile
 from output_parser import PersonIntel, person_intel_parser
 
-# def hf_pipeline_helper():
-
-# hf_model = "HuggingFaceM4/tiny-random-LlamaForCausalLM"
-# tokenizer = AutoTokenizer.from_pretrained(hf_model)
-
-# pipeline = transformers.pipeline(
-# 	"text-generation",
-# 	model=hf_model,
-# 	tokenizer=tokenizer,
-# 	torch_dtype=torch.bfloat16,
-# 	trust_remote_code=True,
-# 	device_map="auto",
-# 	max_length=3000,
-# 	do_sample=True,
-# 	top_k=10,
-# 	num_return_sequences=1,
-# 	eos_token_id=tokenizer.eos_token_id
-# )
-# hf_llm = HuggingFacePipeline(pipeline=pipeline, model_kwargs={'temperature':0})
-# hf_llm = VertexAI()
+from langchain_huggingface import HuggingFacePipeline
+from huggingface_hub import InferenceClient
 
 
 def simple_summary() -> PersonIntel:
     summary_template = """
 	given the following information {information} about a person I want you to create:
-	1. a short summary of the person
-	2. two interesting facts about the person
+	1. a short summary
+	2. two interesting facts about them
     3. a topic that may interest the person
     4. 2 creative ice breakers to open a conversation with the person
                 \n{format_instructions}
@@ -52,29 +34,67 @@ def simple_summary() -> PersonIntel:
             "format_instructions": person_intel_parser.get_format_instructions()
         },
     )
-    # TO DO: download a model from hfh/ use huggingchat api/
-    repo_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    llm = HuggingFaceHub(
-        repo_id=repo_id, model_kwargs={"temperature": 0.2, "max_length": 8192}
-    )
 
     openai_llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    hf_chain = LLMChain(prompt=summary_prompt_template, llm=llm, verbose=True)
+    # hf_chain = LLMChain(prompt=summary_prompt_template, llm=llm, verbose=True)
     openai_chain = LLMChain(llm=openai_llm, prompt=summary_prompt_template)
 
-    linkedin_profile_url = lookup_linkedin("Lex Fridman")
+    linkedin_profile_url = "mock"
     linkedin_data = scrape_linkedin_profile(url=linkedin_profile_url)
-    # linkedin_data = get_sample_linkedin_profile()
-    # res = hf_chain.run(information=linkedin_data)
-    # print(res)
-    res = openai_chain.run(information=linkedin_data)
-    parsed_res = person_intel_parser.parse(res)
-    return parsed_res
+    res = openai_chain.invoke(input={"information": linkedin_data})
     print(res)
+    # parsed_res = person_intel_parser.parse(res)
+    return res
+
+def hf_inference_client():
+    summary_template = """
+	given the following information {information} about a person I want you to create:
+	1. a short summary
+	2. two interesting facts about them
+	"""
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+    )
+    
+    repo_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+    llm_client = InferenceClient(model=repo_id, timeout=120)
+    
+    linkedin_profile_url = "mock"
+    linkedin_data = scrape_linkedin_profile(url=linkedin_profile_url)
+    
+    summary_prompt = summary_template.format(
+        information=linkedin_data
+    )
+    
+    response = llm_client.post(
+        json={
+            "inputs": summary_prompt,
+            "parameters": {"max_new_tokens": 500},
+            "task": "text-generation",
+        },
+    )
+
+    response_text = json.loads(response.decode())[0]["generated_text"]
+    print(response_text)
+    
+    
+# def hf_summary_from_model_id():
+#     llm = HuggingFacePipeline.from_model_id(
+#     model_id="microsoft/Phi-3-mini-4k-instruct",
+#     task="text-generation",
+#     pipeline_kwargs={
+#         "max_new_tokens": 100,
+#         "top_k": 50,
+#         "temperature": 0.1,
+#     },
+#     )
+#     return llm.invoke("Short story")
 
 
 if __name__ == "__main__":
     load_dotenv()
     os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HFH_API_TOKEN")
-    simple_summary()
+    # simple_summary()
+    print(hf_inference_client())
--- ice_breaker/third_parties/linkedin.py
+++ ice_breaker/third_parties/linkedin.py
@@ -2,37 +2,32 @@ import os
 import requests
 
 
-def scrape_linkedin_profile(url: str):
+def scrape_linkedin_profile(url: str, mock: bool = True):
     """
     Scrapes a LinkedIn profile.
     Manually scrapes the information from the LinkedIn profile.
     """
-    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
-    header_dict = {"Authorization": f"Bearer {os.getenv('PROXYCURL_TOKEN')}"}
-    params = {
-        "linkedin_profile_url": url,
-        "extra": "include",
-        "github_profile_id": "include",
-        "facebook_profile_id": "include",
-        "twitter_profile_id": "include",
-        "personal_contact_number": "include",
-        "personal_email": "include",
-        "inferred_salary": "include",
-        "skills": "include",
-        "use_cache": "if-present",
-        "fallback_to_cache": "on-error",
-    }
-    response = requests.get(api_endpoint, params=params, headers=header_dict)
-    response = filter_response(response)
-    return response
-
-
-def get_sample_linkedin_profile():
-    """
-    Uses a predownloaded JSON LinkedIn profile.
-    """
-    lf_gist = "https://gist.githubusercontent.com/haichangsi/8c5dd133aaeb56afc5c4f0fff5b72626/raw/a529da2dc0e0b874a5d3526de033edab8a1629ff/lex-friedman-langchain"
-    response = requests.get(lf_gist)
+    
+    if mock:
+        lf_gist = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"
+        response = requests.get(lf_gist, timeout=10)
+    else:  
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header_dict = {"Authorization": f"Bearer {os.getenv('PROXYCURL_TOKEN')}"}
+        params = {
+            "linkedin_profile_url": url,
+            # "extra": "include",
+            # "github_profile_id": "include",
+            # "facebook_profile_id": "include",
+            # "twitter_profile_id": "include",
+            # "personal_contact_number": "include",
+            # "personal_email": "include",
+            # "inferred_salary": "include",
+            # "skills": "include",
+            # "use_cache": "if-present",
+            # "fallback_to_cache": "on-error",
+        }
+        response = requests.get(api_endpoint, params=params, headers=header_dict, timeout=10)
     response = filter_response(response)
     return response
 
@@ -51,6 +46,8 @@ def filter_response(response):
     }
     if data.get("groups"):
         for group_dict in data.get("groups"):
-            group_dict.pop("profile_pic_url")
+            group_dict.pop("profile_pic_url", None)
 
     return data
+
+# print(scrape_linkedin_profile("https://www.linkedin.com/in/lexfridman/"))
