efbe87382d89eed11a1744ddd8fd41eee3b3dee9
Kacper Stefański <kacper.stefanski99@gmail.com>
2024-07-14 13:23:43 +0200

    add retrieval tag chain using LCEL

--- vector-dbs/main.py
+++ vector-dbs/main.py
@@ -7,8 +7,14 @@ from langchain_pinecone import PineconeVectorStore
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.runnables import RunnablePassthrough
 
 load_dotenv()
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 
 if __name__ == "__main__":
     print("Retrieving...")
@@ -34,3 +40,28 @@ if __name__ == "__main__":
 
     result = retrieval_chain.invoke(input={"input": query})
     print(result)
+
+    template = """Use the following pieces of context to answer the question at the end.
+	If you don't know the answer, just say that you don't know, don't try to make up an answer.
+	Use three sentences maximum and keep the answer as concise as possible.
+	Always say "thanks for asking!" at the end of the answer.
+
+	{context}
+
+	Question: {question}
+
+	Helpful Answer:"""
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {
+            "context": vector_store.as_retriever() | format_docs,
+            "question": RunnablePassthrough(),
+        }
+        | custom_rag_prompt
+        | llm
+    )
+
+    res = rag_chain.invoke(query)
+    print(res)
